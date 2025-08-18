77b3077a3e0c9ca89c19c17c74ed552c05473d20
BoxuanZhou0032 <bzho0032@student.monash.edu>
2025-08-18 18:46:00 +1000

    enhance form validation by adding required attributes and input constraints

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -7,7 +7,13 @@
           <div class="row mb-3">
             <div class="col-md-6 col-sm-6">
               <label for="username" class="form-label">Username</label>
-              <input type="text" class="form-control" id="username" v-model="formData.username" />
+              <input
+                type="text"
+                class="form-control"
+                id="username"
+                required
+                v-model="formData.username"
+              />
             </div>
             <div class="col-md-6 col-sm-6">
               <label for="password" class="form-label">Password</label>
@@ -15,6 +21,8 @@
                 type="password"
                 class="form-control"
                 id="password"
+                minlength="4"
+                maxlength="10"
                 v-model="formData.password"
               />
             </div>
@@ -26,6 +34,7 @@
                   type="checkbox"
                   class="form-check-input"
                   id="isAustralian"
+                  required
                   v-model="formData.isAustralian"
                 />
                 <label class="form-check-label" for="isAustralian">Australian Resident?</label>
@@ -33,7 +42,7 @@
             </div>
             <div class="col-md-6 col-sm-6">
               <label for="gender" class="form-label">Gender</label>
-              <select class="form-select" id="gender" v-model="formData.gender">
+              <select class="form-select" id="gender" required v-model="formData.gender">
                 <option value="male">Male</option>
                 <option value="female">Female</option>
                 <option value="other">Other</option>
@@ -46,6 +55,9 @@
               class="form-control"
               id="reason"
               rows="3"
+              minlength="10"
+              maxlength="200"
+              required
               v-model="formData.reason"
             ></textarea>
           </div>
