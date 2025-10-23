7e06f1c4b47f39fc1c2ea595b797b0fe110fe2c3
BoxuanZhou0032 <bzho0032@student.monash.edu>
2025-10-24 02:49:53 +1100

    Trigger deployment

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/FIT5032/' : '/',
-}
+export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/FIT5032/' : '/',
+})
